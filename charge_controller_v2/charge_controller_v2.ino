b496b0206be40579c1df6ced0b5255f91747a999
Michael Shea <michael.j.shea@maine.edu>
2016-11-14 21:10:47 -0500

    11/14/16 - Up to date charge controller and source selector code

---
+++ charge_controller_v2/charge_controller_v2.ino
@@ -0,0 +1,285 @@
+// Only have 8 bits of precision for duty cycle
+
+// Pin labels
+// ----------
+// Analog mesurement pin
+#define OUTPUT_VOLT_PIN     A5
+// Push button pins
+#define OFF_MODE_BUTTON     A4
+#define HIGH_MODE_BUTTON    A3
+#define LOW_MODE_BUTTON     A2
+#define MANUAL_MODE_BUTTON  A1
+// Potentiometer pin
+#define MANUAL_MODE_POT     A0
+// PWM pin
+#define PWM_PIN             3
+// LED pins
+#define OFF_MODE_LED        4
+#define HIGH_MODE_LED       5
+#define LOW_MODE_LED        6
+#define MANUAL_MODE_LED     7
+
+// State holders
+// -------------
+// Source selector operating mode
+enum operating_mode {off, high, low, manual};
+enum operating_mode current_operating_mode = off;
+
+// Global variables
+// ----------------
+// Output voltage reference value
+const float high_mode_output_ref  = 14.0; // Units of volts
+const float low_mode_output_ref   = 13.5; // Units of volts
+float output_ref = 0;
+// Duty cycle
+unsigned int duty_cycle = 0;
+const unsigned int max_duty_cycle = 0.7*255; // 100% duty cycle is equivalent to max_duty_cycle = 255
+// Output voltage value
+float output_val;
+float tolerance = 0.01;
+
+void setup() {
+  // Initializations
+  // ---------------
+  // Analog measurement pin
+  pinMode(OUTPUT_VOLT_PIN, INPUT);
+  // Push button pins
+  pinMode(OFF_MODE_BUTTON, INPUT);
+  pinMode(HIGH_MODE_BUTTON, INPUT);
+  pinMode(LOW_MODE_BUTTON, INPUT);
+  pinMode(MANUAL_MODE_BUTTON, INPUT);
+  // Potentiometer pin
+  pinMode(MANUAL_MODE_POT, INPUT);
+  // PWM pin
+  pinMode(PWM_PIN, OUTPUT);
+  // LED pins
+  pinMode(OFF_MODE_LED, OUTPUT);
+  pinMode(HIGH_MODE_LED, OUTPUT);
+  pinMode(LOW_MODE_LED, OUTPUT);
+  pinMode(MANUAL_MODE_LED, OUTPUT);
+  digitalWrite(OFF_MODE_LED, HIGH);
+  digitalWrite(HIGH_MODE_LED, LOW);
+  digitalWrite(LOW_MODE_LED, LOW);
+  digitalWrite(MANUAL_MODE_LED, LOW);
+
+  // PWM initialization
+  // ------------------
+  TCCR2B = TCCR2B & 0xF8 | 0x01;  // Set the PWM frequency on pin 3 to 32kHz
+
+  // Serial communication initialization
+  // -----------------------------------
+  Serial.begin(9600);
+}
+void loop() {
+  // To Do:
+  // ------
+  // Set up pushbuttons to interrupts
+  // Proturb and observe
+
+  // Constant output voltage regulation
+  // ----------------------------------
+  int i;
+  for (i = 0; i < 10; i++){
+    Update_current_mode();
+    // Update reference voltage if in manual mode
+    if (current_operating_mode == manual) {
+      output_ref = (analogRead(MANUAL_MODE_POT)/1023.0)*20; // Max output of 20V
+    }
+    Update_duty_cycle();
+    delay(50);
+  }
+  
+  /*
+  duty_cycle = (analogRead(MANUAL_MODE_POT)/1023.0)*255;
+  analogWrite(PWM_PIN, duty_cycle);
+  */
+  
+  /*
+  Print_output_ref();
+  Print_output_val();
+  Print_duty_cycle();
+  Serial.print("-----------------------\n");
+  */ 
+}
+
+// Update current mode
+// -------------------
+// Polls the push buttons
+void Update_current_mode(void) {
+  if ((digitalRead(OFF_MODE_BUTTON) == HIGH) && (current_operating_mode != off)) {
+    current_operating_mode = off;
+    duty_cycle = 0;
+    output_ref = 0;
+    Turn_mode_LEDs_off();
+    digitalWrite(OFF_MODE_LED, HIGH);
+  }
+  if ((digitalRead(HIGH_MODE_BUTTON) == HIGH) && (current_operating_mode != high)) {
+    current_operating_mode = high;
+    output_ref = high_mode_output_ref;
+    Turn_mode_LEDs_off();
+    digitalWrite(HIGH_MODE_LED, HIGH);
+  }
+  if ((digitalRead(LOW_MODE_BUTTON) == HIGH) && (current_operating_mode != low)) {
+    current_operating_mode = low;
+    output_ref = low_mode_output_ref;
+    Turn_mode_LEDs_off();
+    digitalWrite(LOW_MODE_LED, HIGH);
+  }
+  if ((digitalRead(MANUAL_MODE_BUTTON) == HIGH) && (current_operating_mode != manual)) {
+    current_operating_mode = manual;
+    output_ref = 0; // Value updated in proceeding code
+    Turn_mode_LEDs_off();
+    digitalWrite(MANUAL_MODE_LED, HIGH);
+  }
+}
+
+// Turn mode LEDs off
+// ------------------
+void Turn_mode_LEDs_off(void) {
+  digitalWrite(OFF_MODE_LED, LOW);
+  digitalWrite(HIGH_MODE_LED, LOW);
+  digitalWrite(LOW_MODE_LED, LOW);
+  digitalWrite(MANUAL_MODE_LED, LOW);
+}
+
+// Print output reference voltage
+// ------------------------------
+void Print_output_ref(void) {
+  Serial.print("Reference voltage = ");
+  Serial.print(output_ref);
+  Serial.print("V\n");
+  delay(500);
+}
+
+// Update duty cycle
+// -----------------
+void Update_duty_cycle(void) {
+  // Read current output voltage val
+  // -------------------------------
+  // Different calibration for each mode to make results more accurate
+  if (current_operating_mode == manual) {
+    output_val = (analogRead(OUTPUT_VOLT_PIN)/1023.0)*5*4.95;
+  }
+  else if (current_operating_mode == high) {
+    output_val = (analogRead(OUTPUT_VOLT_PIN)/1023.0)*5*4.35;
+  }
+  else if (current_operating_mode == low) {
+    output_val = (analogRead(OUTPUT_VOLT_PIN)/1023.0)*5*4.35;
+  }
+
+  // If in off mode, set duty cycle equal to zero
+  if (output_ref == 0) {
+    duty_cycle = 0;
+    analogWrite(PWM_PIN, duty_cycle);
+  }
+  // If output voltage below tolerance, increase duty cycle
+  else if ((output_val <= (output_ref - output_ref * tolerance)) && (duty_cycle < max_duty_cycle)) {
+    duty_cycle++;
+    analogWrite(PWM_PIN, duty_cycle);
+  }
+  // If output voltage above tolerance, decrease duty cycle
+  else if ((output_val >= (output_ref + output_ref * tolerance)) && (duty_cycle > 0)) {
+    duty_cycle--;
+    analogWrite(PWM_PIN, duty_cycle);
+  }
+}
+
+// Print output voltage
+// ------------------------------
+void Print_output_val(void) {
+  Serial.print("Output voltage = "); 
+  Serial.print(output_val);
+  Serial.print("V\n");
+  //delay(500);
+}
+
+// Print duty cycle
+// ------------------------------
+void Print_duty_cycle(void) {
+  Serial.print("Duty cycle = ");
+  Serial.print(duty_cycle);
+  Serial.print("%\n");
+  //delay(500);
+}
+
+/*
+
+float pot_val;
+float out_val;
+float duty_cycle = 0;
+const int DC_max = 75;
+float ref_voltage = 15.0;
+
+void setup() {
+  // Analog measurements
+  //pinMode(POT_pin, INPUT);
+  //pinMode(OUT_pin, INPUT);
+  
+  // PWM stuff
+  //pinMode(PWM_pin, OUTPUT);
+  //Set_PWM_Frequency__MEGA(PWM_pin, 1);
+  //TCCR0B = TCCR0B & 0xF8 | 0x01; // Pin 5; 62.5kHz
+  //Serial.begin(9600);
+}
+void loop() {
+
+  pot_val = (analogRead(POT_pin)/1023.0)*5;
+  out_val = (analogRead(OUT_pin)/1023.0)*5*4.39;
+
+  
+  if (out_val > (ref_voltage+0.1))
+    duty_cycle--;
+  else if ((out_val < (ref_voltage-0.1)) && (duty_cycle < DC_max))
+    duty_cycle++;
+  
+  // Make sure DC is within range
+  if (duty_cycle > DC_max) {
+    analogWrite(PWM_pin, 0);
+    Serial.print("Error!\n");
+    while(1);
+  }
+ 
+  //duty_cycle = pot_val/5.0;
+  //analogWrite(PWM_pin, duty_cycle*256);
+  analogWrite(PWM_pin, (duty_cycle/100)*256);
+  Serial.print("POT: ");  Serial.print(pot_val);
+  Serial.print("  OUT: ");  Serial.print(out_val);
+  Serial.print("   DC: ");   Serial.print(duty_cycle); 
+  Serial.print("\n");
+  delay(10 000);
+}
+*/
+
+/* Set PWM Frequency on Arduino MEGA 2560 */
+// TCCR: Timer Counter Control Register
+// This function will affect the Arduino timers
+/*
+void Set_PWM_Frequency__MEGA(int pin, int prescaler) {
+  byte mode;
+  switch(prescaler) {
+    case 1:     mode = 0x01; break;
+    case 8:     mode = 0x02; break;
+    case 32:    mode = 0x03; break;
+    case 64:    mode = 0x04; break;
+    case 128:   mode = 0x05; break;
+    case 256:   mode = 0x06; break;
+    case 1024:  mode = 0x07; break;
+    default: return;
+  }
+  switch(pin) {
+    case 2:     TCCR3B = TCCR3B & 0xF8 | mode; break; // 32kHz
+    case 3:     TCCR3B = TCCR3B & 0xF8 | mode; break; // 32kHz
+    case 4:     TCCR0B = TCCR0B & 0xF8 | mode; break; // 64kHz
+    case 5:     TCCR3B = TCCR3B & 0xF8 | mode; break; // 32kHz
+    case 6:     TCCR4B = TCCR4B & 0xF8 | mode; break; // 32kHz
+    case 7:     TCCR4B = TCCR4B & 0xF8 | mode; break; // 32kHz
+    case 8:     TCCR4B = TCCR4B & 0xF8 | mode; break; // 32kHz
+    case 9:     TCCR2B = TCCR2B & 0xF8 | mode; break; // 32kHz
+    case 10:    TCCR2B = TCCR2B & 0xF8 | mode; break; // 32kHz
+    case 11:    TCCR1B = TCCR1B & 0xF8 | mode; break; // 32kHz
+    case 12:    TCCR1B = TCCR1B & 0xF8 | mode; break; // 32kHz
+    case 13:    TCCR0B = TCCR0B & 0xF8 | mode; break; // 64kHz
+    default: return;
+  } 
+}
+*/
---
+++ source_selector/source_selector.ino
@@ -0,0 +1,302 @@
+#define AVAILABLE_THRES 8  // Minimum voltage value in units of volts for a source to be considered available
+#define SOURCE_1 1
+#define SOURCE_2 2
+#define SOURCE_3 3
+
+// Pin labels
+// ----------
+// Gate pin labels
+const int source_1_gate_pin = 49;
+const int source_2_gate_pin = 51;
+const int source_3_gate_pin = 53;
+// Voltage measurement pin labels
+const int source_1_volt_pin = A10;
+const int source_2_volt_pin = A9;
+const int source_3_volt_pin = A8;
+// Button pin labels
+const int source_1_button         = 9;
+const int source_2_button         = 8;
+const int source_3_button         = 7;
+const int auto_select_mode_button = 6;
+const int off_mode_button         = 30;
+// LEDs pin labels
+const int error_detected_LED    = 10;
+const int source_1_available_LED    = 43;
+const int source_1_active_LED   = 44;
+const int source_2_available_LED    = 41;
+const int source_2_active_LED   = 42; 
+const int source_3_available_LED    = 39;
+const int source_3_active_LED   = 40;
+const int auto_select_mode_LED  = 37;
+const int off_mode_LED          = 38;
+
+// State holders and priority setting
+// ----------------------------------
+// Source selector operating mode
+enum operating_mode {off, manual_select, auto_select};
+enum operating_mode current_operating_mode = off;
+// Source priority for auto_select mode
+int source_priority[] = {SOURCE_1, SOURCE_2, SOURCE_3}; // List source in order in which they will be chosen
+// Source selector active source
+enum active_source {none, source_1, source_2, source_3};
+enum active_source current_active_source = none;
+
+// Global variables
+// ----------------
+// Voltage measurements
+float source_1_volt_val;
+float source_2_volt_val;
+float source_3_volt_val;
+// Source availability (HIGH = available, LOW = unavailable);
+bool source_1_availability = LOW;
+bool source_2_availability = LOW;
+bool source_3_availability = LOW;
+
+void setup() {
+  // Initialize gate pins
+  pinMode(source_1_gate_pin, OUTPUT);
+  pinMode(source_2_gate_pin, OUTPUT);
+  pinMode(source_3_gate_pin, OUTPUT);
+  // Make sure gates are low
+  digitalWrite(source_1_gate_pin, LOW);
+  digitalWrite(source_2_gate_pin, LOW);
+  digitalWrite(source_3_gate_pin, LOW);
+
+  // Initialize voltage measurements
+  pinMode(source_1_volt_pin, INPUT);
+  pinMode(source_2_volt_pin, INPUT);
+  pinMode(source_3_volt_pin, INPUT);
+  
+  // Initiallize buttons
+  pinMode(source_1_button,          INPUT);
+  pinMode(source_2_button,          INPUT);
+  pinMode(source_3_button,          INPUT);
+  pinMode(auto_select_mode_button,  INPUT);
+  pinMode(off_mode_button,          INPUT);
+
+  // Initiallize LEDs
+  pinMode(error_detected_LED,   OUTPUT);
+  pinMode(source_1_available_LED,   OUTPUT);
+  pinMode(source_1_active_LED,  OUTPUT);
+  pinMode(source_2_available_LED,   OUTPUT);
+  pinMode(source_2_active_LED,  OUTPUT);
+  pinMode(source_3_available_LED,   OUTPUT);
+  pinMode(source_3_active_LED,  OUTPUT);
+  pinMode(auto_select_mode_LED, OUTPUT);
+  pinMode(off_mode_LED,         OUTPUT);
+  // Make sure LEDs are off
+  digitalWrite(error_detected_LED,    LOW);
+  digitalWrite(source_1_available_LED,    LOW);
+  digitalWrite(source_1_active_LED,   LOW);
+  digitalWrite(source_2_available_LED,    LOW);
+  digitalWrite(source_2_active_LED,   LOW);
+  digitalWrite(source_3_available_LED,    LOW);
+  digitalWrite(source_3_active_LED,   LOW);
+  digitalWrite(auto_select_mode_LED,  LOW);
+  digitalWrite(off_mode_LED,          HIGH);
+
+  // Serial communication initialization
+  Serial.begin(9600);
+}
+
+void loop() {
+  // To do
+  // -----
+  // Hoop up sources to interrupt
+  // Hook up "off button" to interrupt
+ 
+
+  Measure_input_voltages();
+  Update_source_availability();
+  Update_source_mode_and_activity();
+
+}
+
+// Source voltage measurement
+// --------------------------
+// Voltage dividers have been calibrated
+void Measure_input_voltages(void) {
+  source_1_volt_val = (analogRead(source_1_volt_pin)/1023.0)*5*10.98;
+  source_2_volt_val = (analogRead(source_2_volt_pin)/1023.0)*5*10.96;
+  source_3_volt_val = (analogRead(source_3_volt_pin)/1023.0)*5*11.3;
+}
+
+// Source voltage serial print
+// ---------------------------
+void Display_input_voltages(void) {
+  Serial.print("-----------------\n");
+  Serial.print("Source 1 = ");Serial.print(source_1_volt_val);Serial.print("V\n");
+  Serial.print("Source 2 = ");Serial.print(source_2_volt_val);Serial.print("V\n");
+  Serial.print("Source 3 = ");Serial.print(source_3_volt_val);Serial.print("V\n");
+}
+
+// Update source availability
+// --------------------------
+// If voltage above AVAILABLE_THRES set as available, if not set as unavailable
+void Update_source_availability(void) {
+  if(source_1_volt_val > AVAILABLE_THRES) {
+    source_1_availability = HIGH;
+    digitalWrite(source_1_available_LED, HIGH);
+  }
+  else {
+    source_1_availability = LOW;
+    digitalWrite(source_1_available_LED, LOW);    
+  }
+  if(source_2_volt_val > AVAILABLE_THRES) {
+    source_2_availability = HIGH;
+    digitalWrite(source_2_available_LED, HIGH);
+  }
+  else {
+    source_2_availability = LOW;
+    digitalWrite(source_2_available_LED, LOW);    
+  } 
+  if(source_3_volt_val > AVAILABLE_THRES) {
+    source_3_availability = HIGH;
+    digitalWrite(source_3_available_LED, HIGH);
+  }
+  else {
+    source_3_availability = LOW;
+    digitalWrite(source_3_available_LED, LOW);    
+  }
+}
+
+// Update source mode and current active source
+// --------------------------------------------
+// Poll all push push buttons and apply corresponding changes to operating mode and active source
+// If operating in auto mode, apply corresponding changes to active source
+void Update_source_mode_and_activity(void) {
+  
+  // Poll push buttons
+  // -----------------
+  // If source 1 button pressed
+  if((digitalRead(source_1_button) == 1) && ((current_operating_mode != manual_select) || (current_active_source != source_1))) {
+    Turn_mode_LEDs_off();
+    current_operating_mode = manual_select;
+    Activate_source_1();
+  }
+  // If source 2 button pressed
+  if((digitalRead(source_2_button) == 1) && ((current_operating_mode != manual_select) || (current_active_source != source_2))) {
+    Turn_mode_LEDs_off();
+    current_operating_mode = manual_select;
+    Activate_source_2();
+  }
+  // If source 3 button pressed
+  if((digitalRead(source_3_button) == 1) && ((current_operating_mode != manual_select) || (current_active_source != source_3))) {
+    Turn_mode_LEDs_off();
+    current_operating_mode = manual_select;
+    Activate_source_3();
+  }
+  // If auto select mode button pressed
+  if((digitalRead(auto_select_mode_button) == 1) && (current_operating_mode != auto_select)) {
+    current_operating_mode = auto_select;
+    // Note that the current active source is updated in the code below
+    Turn_mode_LEDs_off();
+    digitalWrite(auto_select_mode_LED, HIGH);
+  }
+  // If off mode button pressed
+  if((digitalRead(off_mode_button) == 1) && (current_operating_mode != off)) {
+    current_operating_mode = off;
+    current_active_source = none;
+    Turn_sources_off();
+    Turn_mode_and_source_LEDs_off();
+    digitalWrite(off_mode_LED, HIGH);
+  
+  }
+
+  // Update auto select mode source choice
+  // -------------------------------------
+  // If current operating mode is auto select
+  if(current_operating_mode == auto_select) {
+    // Find and select first seen active source based on priority
+    int i;
+    for (i = 0; i < 3; i++){
+      if((source_priority[i] == SOURCE_1) && (source_1_availability == HIGH)) {
+        Activate_source_1();
+        break;
+      }
+      if((source_priority[i] == SOURCE_2) && (source_2_availability == HIGH)) {
+        Activate_source_2();
+        break;
+      }
+      if((source_priority[i] == SOURCE_3) && (source_3_availability == HIGH)) {
+        Activate_source_3();
+        break;
+      }
+    } 
+  }
+}
+
+// Activate source 1
+// -----------------
+// Update state holder, deactivate other sources, and update LED
+void Activate_source_1(void) {
+    current_active_source = source_1;
+    Turn_sources_off();
+    delay(250); // Prevent backfeeding from different active source which causes oscillation in auto select mode
+    digitalWrite(source_1_gate_pin, HIGH);
+    Turn_source_LEDs_off();
+    digitalWrite(source_1_active_LED, HIGH);
+}
+
+// Activate source 2
+// -----------------
+// Update state holder, deactivate other sources, and update LED
+void Activate_source_2(void) {
+    current_active_source = source_2;
+    Turn_sources_off();
+    delay(250); // Prevent backfeeding from different active source which causes oscillation in auto select mode
+    digitalWrite(source_2_gate_pin, HIGH);
+    Turn_source_LEDs_off();
+    digitalWrite(source_2_active_LED, HIGH);
+}
+
+// Activate source 3
+// -----------------
+// Update state holder, deactivate other sources, and update LED
+void Activate_source_3(void) {
+    current_active_source = source_3;
+    Turn_sources_off();
+    delay(250); // Prevent backfeeding from different active source which causes oscillation in auto select mode
+    digitalWrite(source_3_gate_pin, HIGH);
+    Turn_source_LEDs_off();
+    digitalWrite(source_3_active_LED, HIGH);
+}
+
+// Turn source LEDs off
+// --------------------
+// Turn the following LEDs off: source 1, source 2, source 3
+// Used for changes in active source
+void Turn_source_LEDs_off(void) {
+  digitalWrite(source_1_active_LED,   LOW);
+  digitalWrite(source_2_active_LED,   LOW);
+  digitalWrite(source_3_active_LED,   LOW);
+}
+
+// Turn mode LEDs off
+// ------------------
+// Turn the following LEDs off: auto select mode, off mode
+// Used for changes in operating mode
+void Turn_mode_LEDs_off(void) {
+  digitalWrite(auto_select_mode_LED,  LOW);
+  digitalWrite(off_mode_LED,          LOW);
+}
+
+// Turn source and mode LEDs off
+// -----------------------------
+// Turn the following LEDs off: source 1, source 2, source 3, auto select mode, off mode
+// Used for changes in operating mode or changes in active source 
+void Turn_mode_and_source_LEDs_off(void) {
+  digitalWrite(source_1_active_LED,   LOW);
+  digitalWrite(source_2_active_LED,   LOW);
+  digitalWrite(source_3_active_LED,   LOW);
+  digitalWrite(auto_select_mode_LED,  LOW);
+  digitalWrite(off_mode_LED,          LOW);
+}
+
+// Turn source off
+// ---------------
+void Turn_sources_off(void) {
+  digitalWrite(source_1_gate_pin, LOW);
+  digitalWrite(source_2_gate_pin, LOW);
+  digitalWrite(source_3_gate_pin, LOW);
+}
